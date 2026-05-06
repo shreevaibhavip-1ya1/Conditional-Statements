6a836eee34a2a99b6d49549bfac923ea9285cf34
shreevaibhavip-1ya1 <shreevaibhavip@gmail.com>
2026-05-06 16:54:58 +0530

    this project is about the conditional statemets

---
+++ traffic lights.py
@@ -0,0 +1,10 @@
+light = input("Enter traffic light (RED / GREEN / YELLOW):")
+# Check for RED
+if light == "RED":
+    print("Stop")
+  # Check for GREEN
+if light == "GREEN":
+    print("Go")
+    # Check for YELLOW
+if light == "YELLOW":
+    print("Slow down")
